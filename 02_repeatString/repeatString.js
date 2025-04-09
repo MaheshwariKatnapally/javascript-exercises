0aa211690c9cb86be5129fa8063b8c29d99a0bd8
Maheshwari Katnapally <193353886+MaheshwariKatnapally@users.noreply.github.com>
2025-04-09 13:16:59 -0500

    javascript-excercies commit 1-7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (text, num) {
+  if (num < 0) {
+    return "ERROR";
+  }
+  let temp = "";
+  for (i = 0; i <= num; i++) {
+    temp += text;
+  }
+  return temp;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  const convertedToCelcius = (fahrenheit - 32) * (5 / 9);
+  return Math.round(convertedToCelcius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  const convertedToFahrenheit = celsius * (9 / 5) + 32;
+  return Math.round(convertedToFahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
